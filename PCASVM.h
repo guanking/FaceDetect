cc6b5fe55493dae0f8ef4895ea1a0dae9c92cf4a
guanking19@126.com <guanking19@126.com>
2017-05-06 20:00:26 +0800

    为PCASVM.h文件添加注释

--- PCASVM.h
+++ PCASVM.h
@@ -13,22 +13,64 @@ using namespace cv::ml;
 class PCASVM
 {
 public:
+	/**
+	 *@param facePath 人脸文件路径
+	 *@param faceBegin 人脸图片开始序号
+	 *@param faceEnd 人脸图片结束序号
+	 *@param notfacePath 非人脸文件路径
+	 *@param notfaceBegin 非人脸图片开始序号
+	 *@param norfaceEnd 非人脸图片结束序号
+	 */
 	PCASVM(string facePath,int faceBegin,int faceEnd,string notfacePath,int notfaceBegin,int notfaceEnd);
+	/**
+	 *析构函数
+	 */
 	virtual ~PCASVM();
+	/**
+	 *使用PCA对图片进行成分提取，用训练好的SVM进行检测
+	 *该图片是否为人脸
+	 *@param faceImg 待检测的图片
+	 */
 	bool isFace(Mat faceImg);
+	/**
+	 *和isFace（Mat）功能一样
+	 *@param facePath 待检测的图片的路径
+	 */
 	bool isFace(string facePath);
 protected:
+	/**
+	 *读取人脸和非人脸图像，并对图像进行扁平化处理
+	 *生成每个图像对应的标签
+	 */
 	void initFlatImagesAndLabels();
+	/**
+	 *使用PCA对扁平化后的图像数据进行主成分提取，生成SVM所需的训练数据
+	 */
 	void initPCA();
+	/**
+	 *主成分提取完成之后使用生成的数据惊醒SVM 训练
+	 */
 	void initSVM();
 private:
 	string facePath,notfacePath;
 	int faceBegin,notfaceBegin,faceEnd,notfaceEnd;
 	Ptr<PCA> pca;
 	Ptr<SVM> svm;
+	/**
+	 *扁平化后的图像数据
+	 */
 	Mat flatImgs;
+	/**
+	 *使用PCA提取后的训练数据
+	 */
 	Mat trainData;
+	/**
+	 *每个图像的标签（标识是否为人脸）
+	 */
 	Mat trainLabels;
+	/**
+	 *训练所使用图片的尺寸
+	 */
 	int normalCols;
 	int normalRows;
 };
