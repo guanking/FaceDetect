2af1f66cddc46edc295ae355b6ef3162f2c07d62
guanking19@126.com <guanking19@126.com>
2017-05-17 12:38:23 +0800

    统一图片文件尺寸

--- normalFormat.cpp
+++ normalFormat.cpp
@@ -9,42 +9,66 @@
 
 using namespace std;
 using namespace cv;
-bool normalFormat(string imgDir,int beginIndex,int endIndex,string suffix = ".png")
+
+/**
+ *统一图片大小
+ *
+ *@param imgDir 要转换的图片文件所在的文件夹
+ *@param beginIndex 图片的开始序号
+ *@param endIndex 图片的结束序号
+ *@param standardImg 作为标准的图片
+ *@param suffix 所要处理的文件的后缀名
+ */
+bool normalFormat(string imgDir,int beginIndex,int endIndex,string standardImg = "",string suffix = ".png")
 {
 	Mat img;
 	char imgPath[256];
 	Size size;
-	bool first = true;
+	bool hasStandard = false;
+	if(standardImg.length() != 0)
+	{
+		hasStandard = true;
+		img = imread(standardImg);
+		if(img.empty())
+		{
+			cout<<standardImg<<" is empty !"<<endl;
+			return false;
+		}
+		size.width = img.cols;
+		size.height = img.rows;
+	}
 	for(int i = beginIndex; i <= endIndex; i++)
 	{
 		sprintf(imgPath,"%s%d%s",imgDir.c_str(),i,suffix.c_str());
 		img = imread(imgPath);
-		cout<<"Process \""<<imgPath<<"\" ... ";
+		cout<<endl<<"Process \""<<imgPath<<"\" ... ";
 		if(img.empty())
 		{
 			cout<<imgPath<<" is empty !"<<endl;
 			return false;
 		}
-		if(first)
+		if(!hasStandard)
 		{
 			size = Size(img.cols,img.rows);
-			first = false;
+			hasStandard = true;
 		}
 		else
 		{
-			cout<<"resize"<<img.size;
+			cout<<"resize "<<img.cols<<" X "<<img.rows;
 			resize(img,img,size);
-			cout<<" to "<<img.size;
+			cout<<" to "<<img.cols<<" X "<<img.rows;
 		}
 		imwrite(imgPath,img);
 	}
+
+	cout<<"\n\nUse standard size "<<size.width<<" X "<<size.height<<endl;
 	return true;
 }
 int main(int argc,char* argv[])
 {
 	if(argc < 4)
 	{
-		cout<<"Please input legal params : imgDir,beginIndex,endIndex."<<endl;
+		cout<<"Please input legal params : imgDir,beginIndex,endIndex,standardImg = \"\",suffix = .png"<<endl;
 		return 0;
 	}
 	string imgDir(argv[1]);
@@ -58,7 +82,28 @@ int main(int argc,char* argv[])
 	cout<<"imgDir : "<<imgDir<<endl;
 	cout<<"BeginIndex : "<<beginIndex<<endl;
 	cout<<"EndIndex : "<<endIndex<<endl;
-	normalFormat(imgDir,beginIndex,endIndex);
+	bool result = false;
+	if(argc == 5)
+	{
+		result = normalFormat(imgDir,beginIndex,endIndex,argv[4]);
+	}
+	else if(argc == 6)
+	{
+		result = normalFormat(imgDir,beginIndex,endIndex,argv[4],argv[5]);
+	}
+	else
+	{
+		result = normalFormat(imgDir,beginIndex,endIndex);
+	}
+
+	if(!result)
+	{
+		cout<<"Ternimal ! translate failed !"<<endl;
+	}
+	else
+	{
+		cout<<endl<<"Translate finish !"<<endl;
+	}
 	return 0;
 }
 
